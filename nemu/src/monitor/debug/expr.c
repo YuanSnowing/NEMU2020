266c635e99a13b76fdccb9507f17a9c7615c8b22
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 07:24:42 +0000

    >  compile NEMU 3018218062 simpleedu Linux simpleedu 4.15.0-115-generic #116-Ubuntu SMP Wed Aug 26 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:24:42 up 1 day, 16:38,  1 user,  load average: 1.09, 1.02, 0.90 61aac68d203dcfefd7ffe4e2880ce89117b3ce3e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -155,9 +155,9 @@ int dominant(int p, int q){
 		else if(tokens[i].type == '(') flag ++;
 		else if(tokens[i].type == NUM) continue;
 		else if(tokens[i].value >= oppri && flag == 0) oppri = tokens[i].value, op = i;
-		printf("haha-%d %d\n",i,op);
+		printf("haha %d %d\n",i,op);
 	}
-	if(op == -1) succ = false;
+	if(op == -1) *succ = false;
 	return op;
 }
 
