f77e3f8e5e07fa92824230a74417907cf0cf3eeb
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-24 00:28:51 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:28:51 up  1:25,  1 user,  load average: 0.47, 0.30, 0.26 6b2589602e74112a352ab71beb9a05d54db919c8

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -2,19 +2,19 @@
 
 #define instr pop
 
+static void do_execute() {
+    if(DATA_BYTE != 1){
+        OPERAND_W(op_src, swaddr_read(cpu.esp, DATA_BYTE));
+        REG(R_ESP) += DATA_BYTE;
+    }
+	print_asm_template1();
+}
+
 
 // reg is 16 32
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-
-static void do_execute() {
-    OPERAND_W(op_src, MEM_R(REG(R_ESP)));
-    MEM_W(REG(R_ESP), 0);
-    REG(R_ESP) += DATA_BYTE;
-	print_asm_template1();
-}
 make_instr_helper(r);
 make_instr_helper(rm);
-
 #endif
 
 #include "cpu/exec/template-end.h"
