ce1b7ea7f1f54b6161373310b1511a296117b1f7
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 23:15:16 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:15:16 up 11 min,  1 user,  load average: 0.25, 0.28, 0.20 22be5134d7cccbb6871040c6f68c7cb6b411372f

--- nemu/src/cpu/exec/jmp/jmp-template.h
+++ nemu/src/cpu/exec/jmp/jmp-template.h
@@ -5,14 +5,14 @@
 
 static void do_execute() {
 	DATA_TYPE_S tmp = op_src->val;
-	if (op_src->type == OP_TYPE_MEM || op_src->type == OP_TYPE_REG){
-		cpu.eip = tmp - concat (decode_rm_,SUFFIX)(cpu.eip+1) -1;
-		print_asm_template1();
-	}
-	else{
-		print_asm("jmp %x",cpu.eip + 1 + DATA_BYTE + tmp);
+	if (op_src->type == OP_TYPE_IMM){
 		cpu.eip += tmp;
 	}
+	else{
+		cpu.eip = tmp - concat (decode_rm_,SUFFIX)(cpu.eip+1) -1;
+		printf("why here???\n");
+	}
+	print_asm_template1();
 }
 make_instr_helper(i)
 make_instr_helper(rm)
