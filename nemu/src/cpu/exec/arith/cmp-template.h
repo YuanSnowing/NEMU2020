02c96ccdf9776b7c19f9a3a5c510466a9cef6ee7
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 22:57:15 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:57:15 up  1:11,  1 user,  load average: 0.32, 0.23, 0.23 f685ff3f1369baa6c0e59cc4a52b7d1a18b1fd9c

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -3,19 +3,34 @@
 #define instr cmp
 
 static void do_execute () {
+	// DATA_TYPE result = op_dest->val - op_src->val;
+    // int len = (DATA_BYTE<<3) - 1, a , b;
+    // a = (op_dest->val) >> len;
+    // b = (op_src ->val) >> len;
+    // cpu.ZF = !result;
+    // cpu.SF = result >> len;
+	// cpu.CF = op_dest->val < op_src->val;
+    // cpu.OF = (a != b && b == cpu.SF);
+    // result ^= result >> 4;
+    // result ^= result >> 2;
+    // result ^= result >> 1;
+    // cpu.PF = !(result&1);
+	// print_asm_template2();
 	DATA_TYPE result = op_dest->val - op_src->val;
-    int len = (DATA_BYTE<<3) - 1, a , b;
-    a = (op_dest->val) >> len;
-    b = (op_src ->val) >> len;
-    cpu.ZF = !result;
-    cpu.SF = result >> len;
+	int len = (DATA_BYTE << 3) - 1;
 	cpu.CF = op_dest->val < op_src->val;
-    cpu.OF = (a != b && b == cpu.SF);
-    result ^= result >> 4;
-    result ^= result >> 2;
-    result ^= result >> 1;
-    cpu.PF = !(result&1);
+	cpu.SF=result >> len;
+    	int s1,s2;
+	s1=op_dest->val>>len;
+	s2=op_src->val>>len;
+    	cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
+    	cpu.ZF=!result;
+	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
 	print_asm_template2();
+
 }
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
