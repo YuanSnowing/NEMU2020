d7b08ad4822a4d60c2366645a87c6c62ff1241f4
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-24 00:09:12 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:09:12 up  1:05,  1 user,  load average: 0.34, 0.22, 0.20 15fe024dc58a1946b270e050177e0b7393f531f5

--- nemu/src/cpu/exec/jmp/jg-template.h
+++ nemu/src/cpu/exec/jmp/jg-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	DATA_TYPE_S tmp = op_src->val;
     // eip+tmp + length +1
     print_asm("jg %x\n", cpu.eip + tmp + DATA_BYTE + 1);
-    if(cpu.ZF == 0 || cpu.SF == cpu.OF) cpu.eip += tmp;
+    if(cpu.ZF == 0 && cpu.SF == cpu.OF) cpu.eip += tmp;
 }
 
 make_instr_helper(i);
