0ce693f34456e973918b43b51053316f0086ecf0
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-24 14:26:08 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:26:08 up  1:25,  1 user,  load average: 0.27, 0.34, 0.24 5ecf0847d50bf8043adb60d7598f1fda27e82e9c

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -34,7 +34,7 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	 *
 	op_src->simm = ???
 	 */
-	op_src->simm = instr_fetch(eip,DATA_BYTE);
+	op_src->simm = (DATA_TYPE_S)instr_fetch(eip,DATA_BYTE);
 	op_src->val = op_src->simm;
 
 #ifdef DEBUG
