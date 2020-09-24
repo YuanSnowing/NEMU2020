30343b80a5c40a8a9f1a4cec21622f8be08093cc
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-24 13:46:04 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:46:04 up 45 min,  1 user,  load average: 0.20, 0.26, 0.18 94247fd635429cf011b142526897e925f1194f8a

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -3,7 +3,7 @@
 #define instr push
 
 static void do_execute() {
-    op_src->val = (DATA_TYPE_S)op_src->val;
+    if(DATA_BYTE == 1) op_src->val = (int8_t)op_src->val;
     // before push every time
     reg_l(R_ESP) -= 4;
 	swaddr_write(reg_l(R_ESP), 4, op_src->val);
