a11f161e5eb07f51f1da264084aa698b7dfa6a3d
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 19:03:32 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:03:32 up  9:53,  1 user,  load average: 0.35, 0.18, 0.11 bdc29767e512a785aeb67978a77089c494733336

---
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -0,0 +1,12 @@
+#include "cpu/exec/template-start.h"
+#define instr lods
+
+make_helper(concat(lods_, SUFFIX)) {
+	REG(R_EAX) = MEM_R(reg_l(R_ESI));
+	if (cpu.DF == 0) reg_l(R_ESI) += DATA_BYTE;
+	else reg_l(R_ESI) -= DATA_BYTE;
+	print_asm_template1();
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
