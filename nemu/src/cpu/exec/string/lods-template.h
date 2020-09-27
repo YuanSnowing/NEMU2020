c061cd6637ef5c333aa4b219261cda353a07b561
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 21:23:38 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:23:38 up 23 min,  1 user,  load average: 0.22, 0.20, 0.23 2fa008024d7bb0513e29560dc4310062296723d

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -3,6 +3,7 @@
 
 make_helper(concat(lods_, SUFFIX)) {
 	REG(R_EAX) = MEM_R(reg_l(R_ESI));
+    // DF is an odd register
 	if (cpu.DF == 0) reg_l(R_ESI) += DATA_BYTE;
 	else reg_l(R_ESI) -= DATA_BYTE;
 	print_asm("loads%s", str(SUFFIX));
---
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -0,0 +1,25 @@
+#include "cpu/exec/template-start.h"
+
+make_helper(concat(scas_, SUFFIX)) {
+	swaddr_t a = REG(R_EAX);
+	swaddr_t b = MEM_R(reg_l(R_EDI));
+	uint32_t result = a-b;
+	if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
+	else reg_l(R_EDI) -= DATA_BYTE;
+	
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.ZF = !result;
+	cpu.SF = result >> len;
+	cpu.CF = a < b;
+	int aa = a >> len;
+	int bb = b >> len;
+    cpu.OF = (aa != bb && bb == cpu.SF);
+	result ^= result >> 4;
+	result ^= result >> 2;
+	result ^= result >> 1;
+	cpu.PF = !(result&1);
+    print_asm("scas%s", str(SUFFIX));
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
