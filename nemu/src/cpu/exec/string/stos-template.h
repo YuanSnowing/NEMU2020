cfd6ee5a333f3437e53ed409df9787f2a742eb98
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 21:51:20 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:51:20 up 50 min,  1 user,  load average: 0.00, 0.00, 0.02 523db8f7678d76a3e8b40bc5f6ec40a05cc66f65

---
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -0,0 +1,12 @@
+#include "cpu/exec/template-start.h"
+
+make_helper(concat(stos_, SUFFIX)) {
+	MEM_W(reg_l(R_EDI), REG(R_EAX));
+	if (cpu.DF == 0) reg_l(R_EDI) += DATA_BYTE;
+	else reg_l(R_EDI) -= DATA_BYTE;
+    
+	print_asm("stos%s", str(SUFFIX));
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
