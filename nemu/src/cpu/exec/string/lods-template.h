f13e696e12c42bb1095fe8d9c9c0bd3e894a59eb
3018218062-Yuan Xuening <178853801@qq.com>
2020-09-27 19:47:14 +0800

    PA2 part2 3QAQ

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -5,7 +5,7 @@ make_helper(concat(lods_, SUFFIX)) {
 	REG(R_EAX) = MEM_R(reg_l(R_ESI));
 	if (cpu.DF == 0) reg_l(R_ESI) += DATA_BYTE;
 	else reg_l(R_ESI) -= DATA_BYTE;
-	print_asm_template1();
+	print_asm("loads%s", str(SUFFIX));
 	return 1;
 }
 
