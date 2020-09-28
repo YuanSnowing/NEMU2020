c9b3f6712cb5cc653d8300b7fa6ba1347754b8ec
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 11:42:47 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:42:47 up 23 min,  1 user,  load average: 0.23, 0.18, 0.17 57f338459d4d0d7d7015b4933fee9e9fa8b93779

--- nemu/src/cpu/exec/jmp/ret-template.h
+++ nemu/src/cpu/exec/jmp/ret-template.h
@@ -6,7 +6,7 @@ make_helper(concat(ret_n_, SUFFIX)) {
 	cpu.eip = MEM_R(reg_l(R_ESP));
     reg_l(R_ESP) += DATA_BYTE;
 	print_asm("ret\n");
-	return 1;
+	return 0;
 }
 
 make_helper(concat(ret_i_, SUFFIX)){
@@ -14,8 +14,8 @@ make_helper(concat(ret_i_, SUFFIX)){
 	cpu.eip = MEM_R(REG(R_ESP));
 	cpu.eip &= 0xffff;
 	REG(R_ESP) += 2 + haha;
-	print_asm("ret 0x%x", haha);
-	return 0;
+	print_asm_template1();
+	return 1;
 }
 
 #include "cpu/exec/template-end.h"
