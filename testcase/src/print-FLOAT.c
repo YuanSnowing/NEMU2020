ea75f60f116be2909ff0ff7bfbd67612adbc5fe9
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 16:48:01 +0800

    >  run 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:48:01 up  5:02,  1 user,  load average: 0.27, 0.23, 0.19 51ce29d5b115d5ef8d7b2588da03553c35bc09ac

--- testcase/src/print-FLOAT.c
+++ testcase/src/print-FLOAT.c
@@ -14,6 +14,7 @@ int main() {
 	printf("%f\n", FLOAT_ARG(0x00013333));
 	printf("%f %d\n", FLOAT_ARG(0xfffecccd), 123456);
 #else
+	nemu_assert(0);
 	sprintf(buf, "%f", FLOAT_ARG(0x00010000));
 	nemu_assert(strcmp(buf, "1.000000") == 0);
 	
