840329c02adf134a9861fbefc36f287c13cd7450
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 21:03:51 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:03:51 up  6:24,  1 user,  load average: 0.40, 0.43, 0.25 13fe787e8bb20064a2169fd6ebd453745dd06e6

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -1,7 +1,7 @@
 #include "cpu/exec/template-start.h"
 
 make_helper(concat(movs_n_,SUFFIX)){
-    swaddr_write(reg_l(R_EDI),4,swaddr_read(reg_l(R_ESI),4));
+    swaddr_write(reg_l(R_EDI),DATA_BYTE,swaddr_read(reg_l(R_ESI),DATA_BYTE));
     if (cpu.DF == 0){
         reg_l(R_EDI) += DATA_BYTE;
         reg_l(R_ESI) += DATA_BYTE;
