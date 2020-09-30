b95911f72fd703aa16693f045a5b4b4224fa49e5
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 21:56:15 +0800

    >  run 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:56:15 up  7:17,  1 user,  load average: 0.52, 0.45, 0.39 4476fac86ebd3536f370e87c0c59c6f2c12b1d2

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -18,6 +18,7 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
 
 	char buf[80];
 	int sign = f >> 31;
+	printf("0x%x",sign);
 	sign &= 1;
 	if(sign) f = (~f)+1;
 	int zs = f >> 16;
