dd83feaebbff0afa4f2e9618f24ef4362f8d53da
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 09:14:38 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:14:38 up 30 min,  1 user,  load average: 0.36, 0.36, 0.36 bb56331d7a830f10c47d8a7de8488a8b6723b378

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -119,7 +119,6 @@ static int cmd_bt(char *args){
 	char str[100];
 	for(;ebp; eip = swaddr_read(ebp+4, 4),ebp = swaddr_read(ebp, 4), ++ cnt){
 		getBt(eip, str);
-		printf("hahahahah:%s\n",str);
 		if(str[0] == '\0') break;
 		printf("#%d\t0x%08x:\t%s\targ1:0x%08x arg2:0x%08x arg3:0x%08x arg4:0x%08x\n", cnt, eip, str, 
 			swaddr_read(ebp+8,4), swaddr_read(ebp+12,4), swaddr_read(ebp+16,4), swaddr_read(ebp+20,4));
