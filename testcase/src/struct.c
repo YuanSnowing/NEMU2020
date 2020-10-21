934fb2e15c4ff02bbc122b884f716ef3ee46d0d5
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 23:06:10 +0800

    >  run 3018218062 snow Linux snowNEMU 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:06:10 up 14 min,  1 user,  load average: 0.17, 0.25, 0.19 15c7f106a12a45acea5ba991634c8b84955153ae

--- testcase/src/struct.c
+++ testcase/src/struct.c
@@ -17,9 +17,9 @@ int main() {
 		d.pad1[i] = i + 128;
 		d.pad2[i] = i;
 	}
-
-	struct dummy t = fun(d);
 nemu_assert(0);
+	struct dummy t = fun(d);
+
 	for(i = 0; i < N; i ++) {
 		nemu_assert(t.pad1[i] == i + 128);
 		nemu_assert(t.pad2[i] == i);
