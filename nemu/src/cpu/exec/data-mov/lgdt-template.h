02bd2a2b5bba9db2d43bf308b6417ffe78331a34
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 22:26:34 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:26:34 up 23:09,  1 user,  load average: 0.26, 0.27, 0.27 68df475ccee09213cd9cf7a52c803d643196c8e7

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -4,11 +4,11 @@
 
 static void do_execute() {
 	if (op_src -> size == 2)
-		cpu.gdtr.base = swaddr_read(op_src -> addr + 2,3,0);
+		cpu.gdtr.base = swaddr_read(op_src -> addr + 2,3,R_DS);
 	else{
-		cpu.gdtr.base = swaddr_read(op_src -> addr + 2,4,0);
+		cpu.gdtr.base = swaddr_read(op_src -> addr + 2,4,R_DS);
 	}
-    cpu.gdtr.limit = swaddr_read(op_src -> addr,2,0);
+    cpu.gdtr.limit = swaddr_read(op_src -> addr,2,R_DS);
 	print_asm_template1();
 }
 
