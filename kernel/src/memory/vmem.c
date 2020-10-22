9ec0b3fd0f97c558007babcedbd2d86a32d27376
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-22 20:08:32 +0800

    >  compile kernel 3018218062 snow Linux snowNEMU 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:08:32 up  3:03,  1 user,  load average: 0.91, 0.79, 0.57 1e9592c1f7a5b4daac4ef898476f58f0379db67f

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -6,15 +6,26 @@
 #define SCR_SIZE (320 * 200)
 
 /* Use the function to get the start address of user page directory. */
-PDE* get_updir();
-
+PDE* get_updir();						// kernel page directory
+static PTE ptable[SCR_SIZE] align_to_page;		// kernel page tables
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	panic("please implement me");
+	// panic("please implement me");
+	PDE *pdir = (PDE *)va_to_pa(get_updir());
+	pdir[0].val = make_pde(va_to_pa(ptable));
+	uint32_t i, st, ed;
+	st = VMEM_ADDR/PAGE_SIZE;
+	ed = (VMEM_ADDR+SCR_SIZE)/PAGE_SIZE;
+	if((VMEM_ADDR+SCR_SIZE)%PAGE_SIZE) ed ++;
+	// fill PTEs
+	for (i = st; i <= ed; ++ i) {
+		ptable[i].val = make_pte(i << 12);
+	}
+	
 }
 
 void video_mapping_write_test() {
