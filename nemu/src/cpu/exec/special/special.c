1a67c1c97e8c8a7b189d05ac37d6bb2b5f6effca
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-22 21:15:52 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:15:52 up  4:11,  1 user,  load average: 0.61, 0.30, 0.26 f37808ecb3e5dcabff613ca767cababa8449c48b

--- nemu/src/cpu/exec/special/special.c
+++ nemu/src/cpu/exec/special/special.c
@@ -32,8 +32,8 @@ make_helper(nemu_trap) {
 		   	break;
 
 		default:{
-			if(cpu.eax == 0) printf("\33[1;31mnemu: HIT GOOD TRAP\33[0m at eip = 0x%08x\n\n", cpu.eip);
-			else printf("\33[1;35mnemu: HIT BAD TRAP\33[0m at eip = 0x%08x\n\n", cpu.eip);
+			if(cpu.eax == 0) printf("\33[1;32mnemu: HIT GOOD TRAP\33[0m at eip = 0x%08x\n\n", cpu.eip);
+			else printf("\33[1;31mnemu: HIT BAD TRAP\33[0m at eip = 0x%08x\n\n", cpu.eip);
 			nemu_state = END;
 		}
 	}
