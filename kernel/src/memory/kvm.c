963536b48115f371a4e8013331d7756668bac226
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 17:01:30 +0800

    >  compile kernel 3018218062 snow Linux snowNEMU 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:01:30 up 11:19,  1 user,  load average: 0.30, 0.07, 0.02 395fd65364e31b8bf08ab24145f856cafc282ee9

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -2,7 +2,7 @@
 #include "x86.h"
 #include "memory.h"
 #include <string.h>
-
+#include <stdio.h>
 static PDE kpdir[NR_PDE] align_to_page;						// kernel page directory
 static PTE kptable[PHY_MEM / PAGE_SIZE] align_to_page;		// kernel page tables
 
@@ -12,6 +12,7 @@ PDE* get_kpdir() { return kpdir; }
 void init_page(void) {
 	CR0 cr0;
 	CR3 cr3;
+	printf("%x\n",&cr0);
 	PDE *pdir = (PDE *)va_to_pa(kpdir);
 	PTE *ptable = (PTE *)va_to_pa(kptable);
 	uint32_t pdir_idx;
