8d241674c956c1e44c97e1b8e5471df346b0b0f0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 17:17:22 +0800

    >  compile kernel 3018218062 snow Linux snowNEMU 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:17:22 up 11:35,  1 user,  load average: 0.14, 0.08, 0.03 fcff6013cde6ae59f0fa0c658bfef6e0ec56a859

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -67,7 +67,6 @@ void init_page(void) {
 	cr0.val = read_cr0();
 	cr0.paging = 1;
 	write_cr0(cr0.val);
-	 printf("%x\n",&cr0);
 }
 
 /* GDT in the kernel's memory, whose virtual memory is greater than 0xC0000000. */
