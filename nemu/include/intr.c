aa4f0ce72ce46e24302dcdd0414fd86e6a53d813
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 14:17:11 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:17:11 up  4:01,  1 user,  load average: 0.33, 0.30, 0.23 2fc3ec59be926d0decf40183921889fae106ae14

--- nemu/include/intr.c
+++ nemu/include/intr.c
@@ -5,13 +5,16 @@ void raise_intr(uint8_t NO){
 	/* TODO: Trigger an interrupt/exception with NO
 	 * use NO to index the IDT
 	 */
-	printf("NO: %d\n", NO);
+
+	Assert((NO<<3) <= cpu.idtr.limit, "Interrupt id to0 large");
+	// printf("NO: %d\n", NO);
 	Gate_info gate;
 	idt_des = &gate;
 	
    	lnaddr_t pid = cpu.idtr.base + (NO<<3);
    	idt_des->fst = lnaddr_read(pid, 4);
 	idt_des->sec = lnaddr_read(pid+4, 4);
+	
 	reg_l(R_ESP) -= 4;
 	swaddr_write(reg_l(R_ESP), 4, cpu.EFLAGS, R_SS);
 
