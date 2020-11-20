a3a69473b9984a02601949c3af0e035e20dece61
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 23:10:44 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:10:44 up  7:30,  1 user,  load average: 0.27, 0.34, 0.35 6cb6fd8530d5b3794b770323fb680f651a90ee35

--- nemu/include/intr.c
+++ nemu/include/intr.c
@@ -27,7 +27,7 @@ void raise_intr(uint8_t NO){
 
 	sreg_set(R_CS);
     // printf("hahahahah");
-	// printf("eip to %x\n", cpu.cs.base + idt_desc -> offset1 + (idt_desc -> offset2 << 16));
+	printf("eip to %x\n", cpu.cs.base + idt_des -> offset1 + (idt_des -> offset2 << 16));
 	cpu.eip = cpu.cs.base + idt_des -> offset1 + (idt_des -> offset2 << 16);
 	 
     /* Jump back to cpu_exec() */
