777680a16114a8bc10ffc0d6b8c58d4ed63dd4ed
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 12:47:57 +0800

    >  run 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:47:57 up  2:31,  1 user,  load average: 0.13, 0.15, 0.16 aa1cba033a444994223e099685079871a1a779a7

--- testcase/src/hello-inline-asm.c
+++ testcase/src/hello-inline-asm.c
@@ -6,8 +6,8 @@ int main() {
 	asm volatile (  "movl $4, %eax;"	// system call ID, 4 = SYS_write
 					"movl $1, %ebx;"	// file descriptor, 1 = stdout
 					"movl $str, %ecx;"	// buffer address
-					"movl $14, %edx;"	// length
-					"int $0x80");
-
+					"movl $14, %edx;");	// length
+	set_bp();
+	asm volatile("int $0x80");
 	return 0;
 }
