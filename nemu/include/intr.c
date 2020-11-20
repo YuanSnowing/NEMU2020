8f40aed292b2675c22ca5f7980e8869068c94f96
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 10:41:06 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:41:06 up 25 min,  1 user,  load average: 0.55, 0.29, 0.16 a8c4432528ce0e62e9930808c3bd3feb5078e2c

--- nemu/include/intr.c
+++ nemu/include/intr.c
@@ -1,11 +1,19 @@
 #include <setjmp.h>
 jmp_buf jbuf;
+
 void raise_intr(uint8_t NO){
 	/* TODO: Trigger an interrupt/exception with NO
 	 * use NO to index the IDT
 	 */
+	reg_l(R_ESP) -= 4;
+	swaddr_write(reg_l(R_ESP), 4, cpu.eip, R_SS);
 
-     
-    /* Jump back to cpu_exec() */
+	reg_l(R_ESP) -= 4;
+	swaddr_write(reg_l(R_ESP), 4, cpu.cs.selector, R_SS);
+	
+	reg_l(R_ESP) -= 4;
+	swaddr_write(reg_l(R_ESP), 4, cpu.EFLAGS, R_SS);
+	
+	/* Jump back to cpu_exec() */
     longjmp(jbuf, 1);
 }
