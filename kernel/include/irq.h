97bbc325230b351567b0fe2761e52548cd51dc8e
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 13:05:49 +0800

    >  compile kernel 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:05:49 up  2:49,  1 user,  load average: 0.19, 0.19, 0.18 46eaf47d53e79fad14bfdea4c0300367c61bc1fa

--- kernel/include/irq.h
+++ kernel/include/irq.h
@@ -9,7 +9,8 @@
  */
 
 typedef struct TrapFrame {
-	uint32_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, old_esp, edi, ebp;
+	uint32_t edi, esi, ebp, old_esp, ebx, edx, ecx, eax;
+	uint32_t eip, error_code, eflags, cs;
 	int32_t irq;
 } TrapFrame;
 
