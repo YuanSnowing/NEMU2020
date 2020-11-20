b519df89270fdc3164ae3078567faaf407c6ab06
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 14:00:46 +0800

    >  compile kernel 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:00:46 up  3:44,  1 user,  load average: 0.19, 0.22, 0.18 6bf2215ebb8ebf4d45ab30ccbd66398ec4292dfe

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -33,7 +33,7 @@ void irq_handle(TrapFrame *tf) {
 	 * to match the trap frame built during `do_irq.S'. Remove the
 	 * following line after you are done.
 	 */
-	panic("Have you re-organized the `TrapFrame' structure?");
+	// panic("Have you re-organized the `TrapFrame' structure?");
 
 	int irq = tf->irq;
 	asm volatile(".byte 0xd6" : : "a"(2),"c"(tf->ecx),"d"(tf->edx));
