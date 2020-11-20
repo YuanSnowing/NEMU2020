386b0630bbad7e538a26a9ab5226b681f1b8fe47
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 14:08:03 +0800

    >  compile kernel 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:08:03 up  3:52,  1 user,  load average: 0.23, 0.25, 0.20 aef167fd389e64adab8a65e63f5c0a3ffdbb05f4

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -36,7 +36,7 @@ void irq_handle(TrapFrame *tf) {
 	// panic("Have you re-organized the `TrapFrame' structure?");
 
 	int irq = tf->irq;
-	asm volatile(".byte 0xd6" : : "a"(irq),"c"(tf->ecx),"d"(tf->edx));
+	// asm volatile(".byte 0xd6" : : "a"(irq),"c"(tf->ecx),"d"(tf->edx));
 	if (irq < 0) {
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
