96dec582431ed1580b0cd3ba67a2cd23173d3a90
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-21 13:15:34 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:15:34 up  2:51,  1 user,  load average: 0.24, 0.36, 0.24 e3bbbc07f6873a9676f1516d5dfb1db9d868df75

--- nemu/src/cpu/exec/jmp/call-template.h
+++ nemu/src/cpu/exec/jmp/call-template.h
@@ -6,17 +6,22 @@
 make_helper(concat(call_i_, SUFFIX)) {
 	// eip pointing at 'call 0xe8',to get length of imm
 	// len = decode_i_SUFFIX(cpu.eip + 1)
+	// int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
+	// // stack is from high addr to low addr, so esp should minus imm's value
+	// // databyte is up to suffix
+	// // esp is the top of stack, set it to the right place(length of call + imm)
+	// reg_l(R_ESP) -= DATA_BYTE;
+	// // write return address at esp, 
+	// // write(target, length, content)
+	// swaddr_write(reg_l(R_ESP), 4, eip+len, R_SS);
+	// // op_src->val = op_src->imm, 
+	// cpu.eip += op_src->val;
 	int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
-	// stack is from high addr to low addr, so esp should minus imm's value
-	// databyte is up to suffix
-	// esp is the top of stack, set it to the right place(length of call + imm)
-	reg_l(R_ESP) -= DATA_BYTE;
-	// write return address at esp, 
-	// write(target, length, content)
-	swaddr_write(reg_l(R_ESP), 4, eip+len, R_SS);
-	// op_src->val = op_src->imm, 
-	cpu.eip += op_src->val;
+    reg_l(R_ESP) -= DATA_BYTE;
+    swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1, R_SS);
+    DATA_TYPE_S imm = op_src -> val;
 	print_asm("call 0x%x", cpu.eip+len+1);
+    cpu.eip += imm;
 	return len+1;
 }
 
@@ -30,7 +35,7 @@ make_helper(concat(call_rm_, SUFFIX)) {
 
 	int len = concat(decode_rm_, SUFFIX)(cpu.eip + 1);
 	reg_l(R_ESP) -= DATA_BYTE;
-	swaddr_write(reg_l(R_ESP) , 4, cpu.eip + len, R_SS);
+	swaddr_write(reg_l(R_ESP) , 4, cpu.eip + len+1, R_SS);
 	DATA_TYPE_S imm = op_src -> val;
 	print_asm("call %x",imm);
 	cpu.eip = imm - len - 1;
