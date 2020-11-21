2dd9141f37bcf9224008c8ff3230c0e57f88c0a7
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-21 10:12:18 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:12:18 up 10:08,  1 user,  load average: 0.25, 0.29, 0.34 fbe8b2f2321987fdca53decd52e3aebf701a3a58

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -60,7 +60,7 @@ make_group(group2_cl_b,
 /* 0xd3 */
 make_group(group2_cl_v,
 	inv, inv, inv, inv, 
-	shl_rm_cl_v, shr_rm_cl_v, inv, sar_rm_cl_v)
+	shl_rm_cl_v, shl_rm_cl_v, inv, sar_rm_cl_v)
 
 /* 0xf6 */
 make_group(group3_b,
@@ -74,7 +74,7 @@ make_group(group3_v,
 
 /* 0xfe */
 make_group(group4,
-	inc_rm_b, dec_rm_b, inv, inv, 
+	inv, inv, inv, inv, 
 	inv, inv, inv, inv)
 
 /* 0xff */
@@ -93,12 +93,12 @@ make_group(group7,
 
 /* TODO: Add more instructions!!! */
 
-helper_fun opcode_table [256] = {
+helper_fun opcode_table [256] = { //2^8, function pointer,某种指令的具体形式
 /* 0x00 */	add_r2rm_b, add_r2rm_v, add_rm2r_b, add_rm2r_v,
 /* 0x04 */	add_i2a_b, add_i2a_v, inv, inv,
 /* 0x08 */	or_r2rm_b, or_r2rm_v, or_rm2r_b, or_rm2r_v,
-/* 0x0c */	or_i2a_b, or_i2a_v, inv, _2byte_esc,
-/* 0x10 */	adc_r2rm_b, adc_r2rm_v, adc_rm2r_b, adc_rm2r_v,
+/* 0x0c */	or_i2a_b, or_i2a_v, inv, _2byte_esc,//0x0f，梁子街指令码
+/* 0x10 */	inv, adc_r2rm_v, adc_rm2r_b, adc_rm2r_v,
 /* 0x14 */	adc_i2a_b, adc_i2a_v, inv, inv,
 /* 0x18 */	sbb_r2rm_b, sbb_r2rm_v, sbb_rm2r_b, sbb_rm2r_v,
 /* 0x1c */	sbb_i2a_b, sbb_i2a_v, inv, inv,
@@ -106,48 +106,48 @@ helper_fun opcode_table [256] = {
 /* 0x24 */	and_i2a_b, and_i2a_v, inv, inv,
 /* 0x28 */	sub_r2rm_b, sub_r2rm_v, sub_rm2r_b, sub_rm2r_v,
 /* 0x2c */	sub_i2a_b, sub_i2a_v, inv, inv,
-/* 0x30 */	xor_r2rm_b, xor_r2rm_v, xor_rm2r_b, xor_rm2r_v,
-/* 0x34 */	xor_i2a_b, xor_i2a_v, inv, inv,
-/* 0x38 */	cmp_r2rm_b, cmp_r2rm_v, cmp_rm2r_b, cmp_rm2r_v,
+/* 0x30 */	xor_r2rm_b, xor_r2rm_v, inv, inv,
+/* 0x34 */	xor_i2a_b,xor_i2a_v,inv,inv,
+/* 0x38 */  cmp_r2rm_b, cmp_r2rm_v, cmp_r2rm_b, cmp_rm2r_v,
 /* 0x3c */	cmp_i2a_b, cmp_i2a_v, inv, inv,
 /* 0x40 */	inc_r_v, inc_r_v, inc_r_v, inc_r_v,
 /* 0x44 */	inc_r_v, inc_r_v, inc_r_v, inc_r_v,
-/* 0x48 */	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
-/* 0x4c */	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
+/* 0x48 */	dec_r_v,dec_r_v , dec_r_v, dec_r_v,
+/* 0x4c */	dec_r_v,dec_r_v , dec_r_v, dec_r_v,
 /* 0x50 */	push_r_v, push_r_v, push_r_v, push_r_v,
 /* 0x54 */	push_r_v, push_r_v, push_r_v, push_r_v,
 /* 0x58 */	pop_r_v, pop_r_v, pop_r_v, pop_r_v,
 /* 0x5c */	pop_r_v, pop_r_v, pop_r_v, pop_r_v,
 /* 0x60 */	pusha_v, popa_v, inv, inv,
-/* 0x64 */	inv, inv, operand_size, inv,
+/* 0x64 */	inv, inv, operand_size, inv,//0x66,
 /* 0x68 */	push_i_v, imul_i_rm2r_v, push_i_b, imul_si_rm2r_v,
 /* 0x6c */	inv, inv, inv, inv,
-/* 0x70 */	inv, inv, jb_i_b, jae_i_b,
+/* 0x70 */	jo_i_b, jno_i_b, jb_i_b, jae_i_b,
 /* 0x74 */	je_i_b, jne_i_b, jbe_i_b, ja_i_b,
-/* 0x78 */	js_i_b, jns_i_b, inv, inv,
+/* 0x78 */	js_i_b, jns_i_b, jp_i_b, jnp_i_b,
 /* 0x7c */	jl_i_b, jge_i_b, jle_i_b, jg_i_b,
-/* 0x80 */	group1_b, group1_v, inv, group1_sx_v, 
+/* 0x80 */	group1_b, group1_v, inv, group1_sx_v, //ModR/M 对opcode进行扩充，指令组的形式。
 /* 0x84 */	test_r2rm_b, test_r2rm_v, xchg_r2rm_b, xchg_r2rm_v,
 /* 0x88 */	mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
-/* 0x8c */	inv, lea, mov_sreg2rm, pop_rm_v,
-/* 0x90 */	xchg_a2r_v, xchg_a2r_v, xchg_a2r_v, xchg_a2r_v,
-/* 0x94 */	xchg_a2r_v, xchg_a2r_v, xchg_a2r_v, xchg_a2r_v,
+/* 0x8c */	inv, lea, mov_sreg2rm, inv,
+/* 0x90 */	nop, xchg_a2r_v, inv, inv,
+/* 0x94 */	inv, inv, inv, inv,
 /* 0x98 */	cwtl_v, cltd_v, inv, inv,
 /* 0x9c */	inv, inv, inv, inv,
 /* 0xa0 */	mov_moffs2a_b, mov_moffs2a_v, mov_a2moffs_b, mov_a2moffs_v,
-/* 0xa4 */	inv, inv, inv, inv,
+/* 0xa4 */	movs_n_b, movs_n_v, inv, inv,
 /* 0xa8 */	test_i2a_b, test_i2a_v, stos_b, stos_v,
 /* 0xac */	lods_b, lods_v, scas_b, scas_v,
 /* 0xb0 */	mov_i2r_b, mov_i2r_b, mov_i2r_b, mov_i2r_b,
 /* 0xb4 */	mov_i2r_b, mov_i2r_b, mov_i2r_b, mov_i2r_b,
-/* 0xb8 */	mov_i2r_v, mov_i2r_v, mov_i2r_v, mov_i2r_v, 
-/* 0xbc */	mov_i2r_v, mov_i2r_v, mov_i2r_v, mov_i2r_v,
+/* 0xb8 */	mov_i2r_v, mov_i2r_v, mov_i2r_v, mov_i2r_v, // 指令-形式-操作后缀
+/* 0xbc */	mov_i2r_v, mov_i2r_v, mov_i2r_v, mov_i2r_v,  // v表示操作数长度，b是byte，v是16or32
 /* 0xc0 */	group2_i_b, group2_i_v, ret_i_v, ret_n_v,
 /* 0xc4 */	inv, inv, mov_i2rm_b, mov_i2rm_v,
 /* 0xc8 */	inv, leave, inv, inv,
 /* 0xcc */	int3, intr, inv, iret,
 /* 0xd0 */	group2_1_b, group2_1_v, group2_cl_b, group2_cl_v,
-/* 0xd4 */	inv, inv, nemu_trap, inv,
+/* 0xd4 */	inv, inv, nemu_trap, inv,// defined by ourself
 /* 0xd8 */	inv, inv, inv, inv,
 /* 0xdc */	inv, inv, inv, inv,
 /* 0xe0 */	inv, inv, inv, inv,
@@ -169,7 +169,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x14 */	inv, inv, inv, inv, 
 /* 0x18 */	inv, inv, inv, inv, 
 /* 0x1c */	inv, inv, inv, inv, 
-/* 0x20 */	mov_cr2r, inv, mov_r2cr, inv, 
+/* 0x20 */	mov_cr2r, inv, mov_r2cr, inv,  
 /* 0x24 */	inv, inv, inv, inv,
 /* 0x28 */	inv, inv, inv, inv, 
 /* 0x2c */	inv, inv, inv, inv, 
@@ -193,22 +193,22 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x74 */	inv, inv, inv, inv,
 /* 0x78 */	inv, inv, inv, inv, 
 /* 0x7c */	inv, inv, inv, inv, 
-/* 0x80 */	inv, inv, jb_i_v, jae_i_v,
+/* 0x80 */	jo_i_v, jno_i_v, jb_i_v, jae_i_v,
 /* 0x84 */	je_i_v, jne_i_v, jbe_i_v, ja_i_v,
-/* 0x88 */	js_i_v, jns_i_v, inv, inv, 
-/* 0x8c */	jl_i_v, jge_i_v, jle_i_v, jg_i_v, 
-/* 0x90 */	inv, inv, inv, inv,
-/* 0x94 */	inv, setne_rm_b, inv, inv,
-/* 0x98 */	inv, inv, inv, inv, 
+/* 0x88 */	js_i_v, jns_i_v, jp_i_v, jnp_i_v, 
+/* 0x8c */	jl_i_v, jge_i_v, jle_i_v, jg_i_v,  
+/* 0x90 */	seto_rm_b, inv, setb_rm_b, inv,
+/* 0x94 */	sete_rm_b, setne_rm_b, inv, inv,
+/* 0x98 */	sets_rm_b, inv, setp_rm_b, inv,  
 /* 0x9c */	inv, inv, inv, inv, 
 /* 0xa0 */	inv, inv, inv, inv, 
 /* 0xa4 */	inv, inv, inv, inv,
 /* 0xa8 */	inv, inv, inv, inv,
 /* 0xac */	shrdi_v, inv, inv, imul_rm2r_v,
 /* 0xb0 */	inv, inv, inv, inv, 
-/* 0xb4 */	inv, inv, inv, inv, 
+/* 0xb4 */	inv, inv, movzb_v, movzw_l, 
 /* 0xb8 */	inv, inv, inv, inv,
-/* 0xbc */	inv, inv, inv, inv,
+/* 0xbc */	inv, inv, movsb_v, movsw_l,
 /* 0xc0 */	inv, inv, inv, inv,
 /* 0xc4 */	inv, inv, inv, inv,
 /* 0xc8 */	inv, inv, inv, inv,
@@ -228,7 +228,7 @@ helper_fun _2byte_opcode_table [256] = {
 };
 
 make_helper(exec) {
-	ops_decoded.opcode = instr_fetch(eip, 1);
+	ops_decoded.opcode = instr_fetch(eip, 1); //取一个字节，指令都为1-2字节 {取址}
 	return opcode_table[ ops_decoded.opcode ](eip);
 }
 
