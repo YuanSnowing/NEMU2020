7775e115498fba4e71023f50590a47a810e434b9
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-21 11:54:36 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:54:36 up  1:30,  1 user,  load average: 0.46, 0.37, 0.28 f735045869433971ff7a721b80b58d59c0852654

--- nemu/src/cpu/exec/jmp/jmp-template.h
+++ nemu/src/cpu/exec/jmp/jmp-template.h
@@ -19,24 +19,56 @@ make_instr_helper(rm)
 #if DATA_BYTE == 4
 extern Sreg_info *sreg_info;
 Sreg_info tmp;
+// make_helper(ljmp){
+//     *sreg_info = tmp;
+//     cpu.eip = instr_fetch(cpu.eip+1, 4) - 7;
+//     cpu.cs.selector = instr_fetch(cpu.eip+1 + 4, 2);
+
+//     uint16_t idx = cpu.cs.selector >> 3;//index of sreg
+// 	lnaddr_t chart_addr = cpu.gdtr.base + (idx << 3);//chart addr
+// 	(sreg_info->p1) = lnaddr_read(chart_addr, 4);
+// 	(sreg_info->p2) = lnaddr_read(chart_addr + 4, 4);
+
+// 	cpu.cs.base = (sreg_info->b1) + ((sreg_info->b2) << 16) + ((sreg_info->b3) << 24);
+// 	cpu.cs.limit = (sreg_info->lim1) + ((sreg_info->lim2) << 16) + (0xfff << 24);
+// 	if ((sreg_info->g) == 1) {	//g=0,1b; g=1,4kb, 2^12
+// 		cpu.cs.limit <<= 12;
+// 	}
+// 	// printf("get here!\n");
+//     print_asm("ljmp 0x%x 0x%x",instr_fetch(cpu.eip+1 + 4, 2),instr_fetch(cpu.eip+1, 4));
+//     return 7;    
+// }
 make_helper(ljmp){
-    *sreg_info = tmp;
-    cpu.eip = instr_fetch(cpu.eip+1, 4) - 7;
-    cpu.cs.selector = instr_fetch(cpu.eip+1 + 4, 2);
+    sreg_info = &tmp;
 
+    uint32_t op1 = instr_fetch(cpu.eip + 1,4);
+    uint16_t op2 = instr_fetch(cpu.eip + 1 + 4,2);
+    
+    cpu.eip = op1 - 7;
+    cpu.cs.selector = op2;
     uint16_t idx = cpu.cs.selector >> 3;//index of sreg
-	lnaddr_t chart_addr = cpu.gdtr.base + (idx << 3);//chart addr
-	(sreg_info->p1) = lnaddr_read(chart_addr, 4);
-	(sreg_info->p2) = lnaddr_read(chart_addr + 4, 4);
+	uint32_t chart_addr = cpu.gdtr.base + (idx << 3);//chart addr
+    
+	sreg_info -> p1 = lnaddr_read(chart_addr, 4);
+	sreg_info -> p2 = lnaddr_read(chart_addr + 4, 4);
 
-	cpu.cs.base = (sreg_info->b1) + ((sreg_info->b2) << 16) + ((sreg_info->b3) << 24);
-	cpu.cs.limit = (sreg_info->lim1) + ((sreg_info->lim2) << 16) + (0xfff << 24);
-	if ((sreg_info->g) == 1) {	//g=0,1b; g=1,4kb, 2^12
-		cpu.cs.limit <<= 12;
-	}
-	// printf("get here!\n");
-    print_asm("ljmp 0x%x 0x%x",instr_fetch(cpu.eip+1 + 4, 2),instr_fetch(cpu.eip+1, 4));
-    return 7;    
+    uint32_t btmp = 0;
+
+	btmp |= ((uint32_t)sreg_info -> b1);
+	btmp |= ((uint32_t)sreg_info -> b2)<< 16;
+	btmp |= ((uint32_t)sreg_info -> b3)<< 24;
+    cpu.cs.base = btmp;
+
+	uint32_t ltmp = 0;
+	ltmp |= ((uint32_t)sreg_info -> lim1);
+	ltmp |= ((uint32_t)sreg_info -> lim2) << 16;
+	ltmp |= ((uint32_t)0xfff) << 24;
+    cpu.cs.limit = ltmp;
+
+
+	if (sreg_info -> g == 1) cpu.cs.limit <<= 12;//G = 0, unit = 1B;G = 1, unit = 4KB
+    print_asm("ljmp %x %x",op2,op1);
+    return 1 + 6;    
 }
 #endif
 
