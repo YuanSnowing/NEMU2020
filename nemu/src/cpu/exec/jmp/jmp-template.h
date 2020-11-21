4b08835bb153410766648b98499d05a3d583c98a
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-21 11:49:56 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:49:56 up  1:25,  1 user,  load average: 0.23, 0.22, 0.22 8875fbc1c14b3ec6e01c21dda096fc2d89b2ed5

--- nemu/src/cpu/exec/jmp/jmp-template.h
+++ nemu/src/cpu/exec/jmp/jmp-template.h
@@ -26,12 +26,12 @@ make_helper(ljmp){
 
     uint16_t idx = cpu.cs.selector >> 3;//index of sreg
 	lnaddr_t chart_addr = cpu.gdtr.base + (idx << 3);//chart addr
-	sreg_info->p1 = lnaddr_read(chart_addr, 4);
-	sreg_info->p2 = lnaddr_read(chart_addr + 4, 4);
+	(sreg_info->p1) = lnaddr_read(chart_addr, 4);
+	(sreg_info->p2) = lnaddr_read(chart_addr + 4, 4);
 
-	cpu.cs.base = sreg_info->b1 + (sreg_info->b2 << 16) + (sreg_info->b3 << 24);
-	cpu.cs.limit = sreg_info->lim1 + (sreg_info->lim2 << 16) + (0xfff << 24);
-	if (sreg_info->g == 1) {	//g=0,1b; g=1,4kb, 2^12
+	cpu.cs.base = (sreg_info->b1) + ((sreg_info->b2) << 16) + ((sreg_info->b3) << 24);
+	cpu.cs.limit = (sreg_info->lim1) + ((sreg_info->lim2) << 16) + (0xfff << 24);
+	if ((sreg_info->g) == 1) {	//g=0,1b; g=1,4kb, 2^12
 		cpu.cs.limit <<= 12;
 	}
 	// printf("get here!\n");
