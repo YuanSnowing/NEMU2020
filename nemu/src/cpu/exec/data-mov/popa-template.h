7d5a21d0276c292a7c4cf2289d3680cf8d4373e5
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-21 10:21:07 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:21:07 up 10:17,  1 user,  load average: 1.82, 1.39, 0.79 b34de2e84e8372f391dac82d54d280b625ee3fe

--- nemu/src/cpu/exec/data-mov/popa-template.h
+++ nemu/src/cpu/exec/data-mov/popa-template.h
@@ -2,25 +2,40 @@
 
 #define instr popa
 
-#ifndef mypop
-#define mypop
-static uint32_t pop(){
+// #ifndef mypop
+// #define mypop
+// static uint32_t pop(){
+//     uint32_t ret = swaddr_read(reg_l(R_ESP),DATA_BYTE,R_SS);
+//     swaddr_write(reg_l(R_ESP),DATA_BYTE,0,R_SS);
+//     reg_l(R_ESP) += DATA_BYTE;
+//     return ret;
+// }
+// #endif
+
+#if DATA_BYTE == 2
+static uint32_t pops_w(){
     uint32_t ret = swaddr_read(reg_l(R_ESP),DATA_BYTE,R_SS);
-    swaddr_write(reg_l(R_ESP),DATA_BYTE,0,R_SS);
+    reg_l(R_ESP) += DATA_BYTE;
+    return ret;
+}
+#endif
+#if DATA_BYTE == 4
+static uint32_t pops_l(){
+    uint32_t ret = swaddr_read(reg_l(R_ESP),DATA_BYTE,R_SS);
     reg_l(R_ESP) += DATA_BYTE;
     return ret;
 }
 #endif
 
 make_helper(concat(popa_,SUFFIX)){
-    REG(R_EDI) = pop();
-    REG(R_ESI) = pop();
-    REG(R_EBP) = pop();
-    REG(R_EBX) = pop(); // five
-    REG(R_EBX) = pop();
-    REG(R_EDX) = pop();
-    REG(R_ECX) = pop();
-    REG(R_EAX) = pop();
+    REG(R_EDI) = concat(pops_,SUFFIX)();
+    REG(R_ESI) = concat(pops_,SUFFIX)();
+    REG(R_EBP) = concat(pops_,SUFFIX)();
+    REG(R_EBX) = concat(pops_,SUFFIX)(); // five
+    REG(R_EBX) = concat(pops_,SUFFIX)();
+    REG(R_EDX) = concat(pops_,SUFFIX)();
+    REG(R_ECX) = concat(pops_,SUFFIX)();
+    REG(R_EAX) = concat(pops_,SUFFIX)();
     print_asm("popa");
     return 1;
 }
