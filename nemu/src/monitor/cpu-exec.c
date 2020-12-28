12c99ac8ba211522efc3573aa0ee5fee4cd7891e
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 22:05:55 +0800

    >  compile NEMU 3018218062 snow Linux snowNEMU 5.4.0-53-generic #59~18.04.1-Ubuntu SMP Wed Oct 21 12:14:56 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:05:55 up 14:08,  1 user,  load average: 0.26, 0.15, 0.15 ce5cbac68c33774966e1a09cc1f2c59418a81f5b

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -52,7 +52,7 @@ void cpu_exec(volatile uint32_t n) {
 	setjmp(jbuf);
 
 	for(; n > 0; n --) {
-		if (cpu.eip >= 0xc0100fb0) printf("hhhhhhhhhhhh edi:0x%8x\n", cpu.edi);
+		if (cpu.eip >= 0xc0100fb0) printf("hhhhhhhhhhhh edi:0x%08x\n", cpu.edi);
 #ifdef DEBUG
 		swaddr_t eip_temp = cpu.eip;
 		if((n & 0xffff) == 0) {
